aca792112685904b7eb19e4ab6544a59e6a6f441
leglars <leglars@yixishangcundeMacBook-Pro.local>
2017-03-11 01:36:38 +1000

    Fix some simple issues

---
+++ src/components/AboutMe.js
@@ -0,0 +1,14 @@
+/**
+ * Created by leglars on 2017/3/10.
+ */
+import React from 'react';
+
+const AboutMe = React.createClass({
+    render: function() {
+        return (
+            <p>I am Nicholas Zheng, a UX designer.</p>
+        )
+    }
+});
+
+export default AboutMe;
