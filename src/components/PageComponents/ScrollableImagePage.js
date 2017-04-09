0191c29abc85ba9d6797176b2a1ad8f38aa1fa4b
leglars <leglars@gmail.com>
2017-04-10 06:33:41 +1000

    Scrollable image frame

---
+++ src/components/PageComponents/ScrollableImagePage.js
@@ -0,0 +1,40 @@
+/**
+ * Created by leglars on 2017/4/10.
+ */
+/**
+ * Created by leglars on 2017/4/4.
+ */
+exports.__esModule = true;
+
+import React from 'react';
+import SwitchableImageFrame from './SwitchableImageFrame';
+
+
+
+
+const ScrollableImagePage = React.createClass({
+    render: function () {
+        const { page, styleId } = this.props
+
+        const size = {
+            width: 800,
+            height: 600,
+        };
+
+        return (
+            <div className={styleId}>
+                <div className="scrollableImageFrame">
+                    <SwitchableImageFrame images={page.images} {...size}/>
+                </div>
+            </div>
+
+        )
+    }
+});
+
+ScrollableImagePage.propTypes = {
+    page: React.PropTypes.object,
+    styleId: React.PropTypes.string
+};
+
+exports.default = ScrollableImagePage;
