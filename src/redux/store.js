7514bb4675e37705211370387f99690731a36ab1
leglars <leglars@gmail.com>
2017-04-14 18:18:29 +1000

    transform browserHistory to hashHistory for GitHub pages

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-import createHistory from 'history/createBrowserHistory';
+import createHistory from 'history/createHashHistory';
 import { routerMiddleware } from 'react-router-redux'
 
 import reducers from './reducers/index';
